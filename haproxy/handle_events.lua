9ad62886d4652c278ab811dea7c861b766f06455
John Stoltenberg <jstoltenberg@605.tv>
2021-04-14 22:38:19 -0700

    turn on loggin haproxy, update lua script

--- haproxy/handle_events.lua
+++ haproxy/handle_events.lua
@@ -1,6 +1,5 @@
 core.register_fetches("handle-events", function(txn)
 
-  -- local encode = require("./urlencode")
   function urlencode(url)
     local char_to_hex = function(c)
       return string.format("%%%02X", string.byte(c))
@@ -14,36 +13,39 @@ core.register_fetches("handle-events", function(txn)
     return url
   end
 
-  local headers = {}
-  for header, values in pairs(txn.http:req_get_headers()) do
-    for i, v in pairs(values) do
-      if headers[header] == nil then
-        headers[header] = v
-      else
-        headers[header] = headers[header] .. ", " .. v
-      end
-    end
-  end
-
+  -- Get headers and Loggly Token
+  local headers = txn.http:req_get_headers()
   local secret = os.getenv("LOGGLY_TOKEN")
   local new_host = "logs-01.loggly.com"
 
+  -- Create Path
   local path = core.concat()
   path:add("/inputs/")
   path:add(secret)
   path:add(".gif")
 
+  -- Append headers to query
   local query = core.concat()
   query:add(tostring(txn.f:query()))
-  query:add("&forwardedFor=")
-  query:add(tostring(txn.f:src()))
+  -- Source IP
+  if txn.f:src() ~= nil then
+    query:add("&forwardedFor=")
+    query:add(tostring(txn.f:src()))
+  end
+  -- HAProxy generated eventtime
   query:add("&eventTime=")
-  query:add(headers["x-request-start"])
-  query:add("&useragent=")
-  query:add(urlencode(headers["user-agent"]))
-  query:add("&referer=")
-  query:add(urlencode(headers["referer"]))
-
+  query:add(headers["x-request-start"][0])
+  -- User Agent
+  if headers["user-agent"] ~= nil then
+    query:add("&useragent=")
+    query:add(urlencode(headers["user-agent"][0]))
+  end
+  -- Referer
+  if headers["referer"] ~= nil then
+    query:add("&referer=")
+    query:add(urlencode(headers["referer"][0]))
+  end
+  -- Build URI
   local uri = core.concat()
   uri:add("https://")
   uri:add(new_host)
